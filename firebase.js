b9519a4dd7b828063e453d102525509bc65a14aa
amir5124 <bocahangon64@gmail.com>
2025-08-03 02:33:07 +0700

    Update firebase.js to load service account from environment variable

--- firebase.js
+++ firebase.js
@@ -1,5 +1,22 @@
-const admin = require("firebase-admin");
-const serviceAccount = require("./serviceAccountKey.json");
+const fs = require('fs');
+const admin = require('firebase-admin');
+
+// Cek apakah file sudah ada, kalau belum generate dari ENV
+const filePath = './serviceAccountKey.json';
+
+if (!fs.existsSync(filePath)) {
+    const base64 = process.env.SERVICE_ACCOUNT_BASE64;
+
+    if (!base64) {
+        console.error('❌ SERVICE_ACCOUNT_BASE64 tidak tersedia di environment variable.');
+        process.exit(1);
+    }
+
+    const jsonContent = Buffer.from(base64, 'base64').toString('utf-8');
+    fs.writeFileSync(filePath, jsonContent);
+}
+
+const serviceAccount = require(filePath);
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
